67e30ea22f9107bbc6b5a9b7be33f50387d74a4f
Masamitsu MURASE <masamitsu.murase@gmail.com>
2013-02-17 03:10:31 +0900

    Add test.

--- test/mruby_hs_regexp.rb
+++ test/mruby_hs_regexp.rb
@@ -1,1 +1,17 @@
 
+# Constant
+assert("HsRegexp::CONSTANT") do
+  HsRegexp::IGNORECASE == 1 and HsRegexp::MULTILINE == 2
+end
+
+
+assert("HsRegexp.new") do
+  HsRegexp.new(".*") and HsRegexp.new(".*", HsRegexp::MULTILINE)
+end
+
+assert("HsRegexp#match") do
+  reg = HsRegexp.new("(https?://[^/]+)[-a-zA-Z0-9./]+")
+  reg.match("http://masamitsu-murase.12345/hoge.html") and
+    reg.match("http:///masamitsu-murase.12345/hoge.html").nil?
+end
+
