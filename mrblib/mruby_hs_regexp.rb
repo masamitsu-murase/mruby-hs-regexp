bf8b3014d772c113a66f3fa395291049ecfb4718
Daniel Bovensiepen <daniel@bovensiepen.net>
2013-02-19 05:56:16 +0000

    Add escape, quote and some tests

--- mrblib/mruby_hs_regexp.rb
+++ mrblib/mruby_hs_regexp.rb
@@ -1,24 +1,55 @@
 class HsRegexp
+  ESCAPE_LIST = {
+    '0x0a' => '\n', '0x09' => '\t',
+    '0x0d' => '\r', '0x0c' => '\f',
+    '0x20' => '\0x20', '#' => '\#',
+    '$' => '\$', '(' => '\(',
+    ')' => '\)', '*' => '\*',
+    '+' => '\+', '-' => '\-',
+    '.' => '\.', '?' => '\?',
+    '[' => '\[', '}' => '\}',
+    ']' => '\]', '^' => '\^',
+    '{' => '\{', '|' => '\|'
+  }
+
   # ISO 15.2.15.6.1
   def self.compile(*args)
     self.new(*args)
   end
 
-  # ISO 15.2.15.7.8
-  attr_reader :source
+  # ISO 15.2.15.6.2
+  def self.escape(str)
+    tmp = str.gsub '\\', '\\\\'
+    ESCAPE_LIST.each { |k, v| tmp = tmp.gsub(k, v) }
+    tmp
+  end
 
+  # ISO 15.2.15.6.4
+  class << self
+    alias :quote :escape
+  end
+
+  # 15.2.15.6.3
   def self.last_match
     return @last_match
   end
 
+  # ISO 15.2.15.7.8
+  attr_reader :source
+
+  # ISO 15.2.15.7.4
   def ===(str)
     return self.match(str) ? true : false
   end
 
+  # ISO 15.2.15.7.5
   def =~(str)
     m = self.match(str)
     return m ? m.begin(0) : nil
   end
+
+  # ISO 15.2.15.7.8
+  attr_reader :source
 end
 
 class HsMatchData
@@ -38,6 +69,12 @@ class HsMatchData
     end
   end
 
+  # ISO 15.2.16.3.1
+  def [](index)
+    d = @data[index]
+    return (d && @string.slice(d[:beg], d[:len]))
+  end
+
   # ISO 15.2.16.3.2
   def begin(index)
     d = @data[index]
@@ -50,10 +87,10 @@ class HsMatchData
     return (d && (d[:beg] + d[:len]))
   end
 
-  # ISO 15.2.16.3.1
-  def [](index)
+  # ISO 15.2.16.3.7
+  def offset(index)
     d = @data[index]
-    return (d && @string.slice(d[:beg], d[:len]))
+    return (d && [ d[:beg], d[:beg] + d[:len] ])
   end
 
   # ISO 15.2.16.3.8
@@ -67,15 +104,8 @@ class HsMatchData
     return @string.slice(0, @data[0][:beg])
   end
 
-  # ISO 15.2.16.3.7
-  def offset(index)
-    d = @data[index]
-    return (d && [ d[:beg], d[:beg] + d[:len] ])
-  end
-
   # ISO 15.2.16.3.13
   def to_s
     return self[0]
   end
 end
-
